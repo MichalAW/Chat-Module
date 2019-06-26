098287ab89894a72e3aff36c53c407d493a5a783
MichalAW <michal.wantaeuco@gmail.com>
2019-06-26 19:58:29 +0200

    bug webpack

--- client/App.js
+++ client/App.js
@@ -14,10 +14,47 @@ class App extends Component {
         super(props);
         this.state = {users: [], messages: [], text: '', name: ''};
     }
+
+    componentDidMount() {
+        socket.on('message', message => this.messageReceive(message));
+        socket.on('update', ({
+            users
+        }) => this.chatUpdate(users));
+    }
+
+    messageReceive(message) {
+        const messages = [message, ...this.state.messages];
+        this.setState({
+            messages
+        });
+    }
+
+    chatUpdate(users) {
+        this.setState({
+            users
+        });
+    }
+
+    handleMessageSubmit(message) {
+        const messages = [message, ...this.state.messages];
+        this.setState({
+            messages
+        });
+        socket.emit('message', message);
+    }
+        
+    handleUserSubmit(name) {
+        this.setState({
+            name
+        });
+        socket.emit('join', name);
+    }
+
     render() {
         return this.state.name !== '' ? this.renderLayout() : this.renderUserForm();
         // <warunek_do_sprawdzenia> ? <przypadek_true> : <przypadek_false>
     }
+
     renderLayout() {
         return (
             <div className={styles.App}>
