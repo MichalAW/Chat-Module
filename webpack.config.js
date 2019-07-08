ff0e737f4b15858cd20ad3d530fb9676ccb2c13f
MichalAW <michal.wantaeuco@gmail.com>
2019-07-08 15:36:42 +0200

    a

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,19 @@ module.exports = {
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
             }
         ]
     },
